3d93fe7c14da1495af3dc44bfc6de53d49027aef
AquaQuokka <101112829+AquaQuokka@users.noreply.github.com>
2023-04-22 12:32:43 +0800

    feat(package): add license 'BSD-3-Clause'

--- setup.py
+++ setup.py
@@ -3,18 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='pyrew',
     version='0.2.2',
+    description='A Python library for writing shorter and more efficient Python code.',
+    url="https://github.com/AquaQuokka/pyrew",
+    author="AquaQuokka",
+    license='BSD-3-Clause',
     packages=find_packages(),
-    install_requires=[
-        "sys",
-        "builtins",
-        "importlib",
-        "os",
-        "contextlib",
-        "logging",
-        "asyncio",
-        "time",
-        "itertools",
-        "threading",
-        "re"
-    ]
+    install_requires=[]
 )
