7fcf6dc33d542ce8caddd26b675b2753e82e2dbd
AquaQuokka <101112829+AquaQuokka@users.noreply.github.com>
2023-04-22 13:43:28 +0800

    ...

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyrew',
-    version='0.4.0',
+    version='0.4.1',
     description='A Python library for writing shorter and more efficient Python code.',
     long_description="Welcome to Pyrew, a simple Python library for writing shorter and more efficient Python code. Simply install and import the module.",
-    url="git+https://github.com/AquaQuokka/pyrew.git",
+    url="https://github.com/AquaQuokka/pyrew",
     author="AquaQuokka",
     license='BSD-3-Clause',
     py_modules=['pyrew'],
