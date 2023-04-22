8c51acc6592390f77d8f73a9f7dd5b9d9c87aa70
AquaQuokka <101112829+AquaQuokka@users.noreply.github.com>
2023-04-22 19:38:48 +0800

    fix(deps): require humanize

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as f:
 
 setup(
     name='pyrew',
-    version='0.11.0',
+    version='0.12.0',
     description='A Python library for writing shorter and more efficient Python code.',
     long_description=long_description,
     url="https://github.com/AquaQuokka/pyrew",
@@ -13,5 +13,5 @@ setup(
     license='BSD-3-Clause',
     py_modules=['pyrew'],
     scripts=['pyrew.py'],
-    install_requires=[],
+    install_requires=["humanize"],
 )
